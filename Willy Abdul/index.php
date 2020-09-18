02c20db022f4632d7d08c454c54eaf9ce0d74757
willyabdul1304 <58999755+willyabdulr@users.noreply.github.com>
2020-09-18 10:21:44 +0700

    Add files via upload

---
+++ Willy Abdul/index.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title>Biodata</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css.css">
+    <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Lobster&family=Slabo+27px&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Slabo+27px&display=swap" rel="stylesheet">
+
+    <style>
+      body {
+        background-color: #f1f1f2;
+}
+    </style>
+  </head>
+  <body>
+ <nav class="navbar navbar-light" style="background-color: #1995ad;">
+       <span class="navbar-brand mb-0 h1" style="margin-left:550px" ><h1 style="font-family: 'Fjalla One', sans-serif;color: white;">BIODATA</h1></span>
+
+</nav>
+        
+<br><br>
+        <!-- Isi Biodata Paling Keren di HTML disini -->
+<div class="container" style="font-family: 'Slabo 27px', serif;
+" >
+  <div class="card kartu">
+    <div class="row">
+      <div class="col-md-4">
+      <div class="foto">
+        <img src="img/1.jpg" class="img-thumbnail" alt="" width="auto" height="auto" >
+      </div>
+      </div>
+      <div class="col-md-8 kertas-biodata">
+        <div class="biodata">
+        <table width="100%" border="0">
+    <tbody><tr>
+        <td valign="top">
+        <table border="0" widtstyle="" e="padding-left: 2px; padding-right: 13px; background-color: # ">
+          <tbody>
+            <tr>
+              <td width="25%" valign="top" class="textt">Nama</td>
+                <td width="2%">:</td>
+                <td>Willy Abdul Rozak</td>
+            </tr>
+             <tr>
+              <td class="textt">No HP</td>
+                <td>:</td>
+                <td>089630992703</td>
+            </tr>
+            <tr>
+              <td class="textt">Tempat, Tanggal Lahir</td>
+                <td>:</td>
+                <td>Bogor, 13 April 2002</td>
+            </tr>
+            <tr>
+              <td class="textt">Cita-Cita</td>
+                <td>:</td>
+                <td>Menjadi Orang Sukses & Ahlul Qur'an</td>
+            </tr>
+            <tr>
+              <td class="textt">Usia</td>
+                <td>:</td>
+                <td>18</td>
+            </tr>
+            <tr>
+              <td valign="top" class="textt">Instagram</td>
+                <td valign="top">:</td>
+                <td>willyabdul1304</td>
+            </tr>
+            <tr>
+              <td valign="top" class="textt">Rayon</td>
+                <td valign="top">:</td>
+                <td>Wikrama 5</td>
+            </tr>
+             <tr>
+              <td valign="top" class="textt">Twitter</td>
+                <td valign="top">:</td>
+                <td>Willy Abdul</td>
+            </tr>
+              <tr>
+              <td class="textt">Jenis Kelamin</td>
+                <td>:</td>
+                <td>Laki-Laki</td>
+            </tr>
+              <tr>
+              <td class="textt">Rombel</td>
+                <td>:</td>
+                <td>RPL XII</td>
+            </tr>
+             <tr>
+              <td valign="top" class="textt">Facebook</td>
+                <td valign="top">:</td>
+                <td>Willy Abdul</td>
+            </tr>
+             <tr>
+              <td class="textt">Alamat</td>
+                <td>:</td>
+                <td>Kp.cilengsir Kec.Cigombong Kab.Bogor</td>
+            </tr>
+             <tr>
+              <td class="textt">Hobi</td>
+                <td>:</td>
+                <td>Basket Ball</td>
+            </tr>
+          <tr>
+              <td class="textt">Asal Sekolah</td>
+                <td>:</td>
+                <td>SMK WIKRAMA 1 GARUT</td>
+            </tr>
+          <tr>
+              <td valign="top" class="textt">Jurusan</td>
+                <td valign="top">:</td>
+                <td>REKAYASA PERANGKAT LUNAK</td>
+            </tr>
+             <tr>
+              <td class="textt">Agama</td>
+                <td>:</td>
+                <td>Islam</td>
+            </tr>
+            <tr>
+              <td class="textt">Email</td>
+                <td>:</td>
+                <td>willyabdul595@gmail.com</td>
+            </tr>
+            <tr>
+              <td valign="top" class="textt">Github</td>
+                <td valign="top">:</td>
+                <td>https://github.com/willyabdulr</td>
+            </tr>
+            <tr>
+              <td valign="top" class="textt">Gtlab</td>
+                <td valign="top">:</td>
+                <td>https://gitlab.com/willyabdulr</td>
+            </tr>
+            
+        </tbody></table>
+        </td>
+    </tr>
+    </tbody></table>
+  </div>
+      </div>
+    </div>
+  </div>
+</div>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
